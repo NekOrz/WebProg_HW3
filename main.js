018cedabdf45f9550b6cc957c4dcf9264f3ddd19
NekOrz <joe1256joe@gmail.com>
2016-10-18 01:37:16 +0800

    Finish requirement - 'POST'
    1. POST /api/body will now return some json.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const router = express.Router();  // eslint-disable-line
 const app = express();
@@ -9,10 +10,13 @@ app.listen(7122, () => {
   console.log('on');
 });
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use('/', router);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'nunjucks');
+
 nunjucks.configure('views', {
   autoescape: true,
   express: app,
@@ -25,4 +29,8 @@ router.get('/', (req, res) => {
 
 router.get('/api/query', (req, res) => {
   res.json(req.query);
+});
+
+router.post('/api/body', (req, res) => {
+  res.json(req.body);
 });
